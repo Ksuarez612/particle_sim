e860fb98aeb9bac2de6b06ec9f4d9d2677ccc054
javierlinero <jl5951@princeton.edu>
2024-09-12 18:18:33 -0400

    changed the bool to border char * for diff border implementations + fixed some of the velocity issues

--- vis.c
+++ vis.c
@@ -1,3 +1,7 @@
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
 #include <GLFW/glfw3.h>
 #include <windows.h>
 #include <GL/glu.h>
@@ -23,12 +27,13 @@ void initOpenGL();
 void draw();
 // void render_text(const char* text, float x, float y, float scale, float r, float g, float b);
 
+const char* border = "square";
+
 
 int num_particles = 300;
 Particle* particles;
 const float GRAVITY = -1.98f;
-const float sphere_radius = 3.0f;
-const bool boolsphere = false; 
+const float sphere_radius = 1.0f;
 const float time_step = 0.01f;
 
 
@@ -106,7 +111,28 @@ int main() {
 // }
 
 void initialize_particles(Particle* particles, int num_particles) {
-    for (int i = 0; i < num_particles; i++) {
+    if(border == "sphere") {
+        int i;
+        for (i = 0; i < num_particles; i++) {
+            float theta = ((float)rand() / RAND_MAX) * 2.0f * M_PI;
+            float phi = ((float)rand() / RAND_MAX) * M_PI;
+
+            particles[i].x = sphere_radius * sin(phi) * cos(theta);
+            particles[i].y = sphere_radius * sin(phi) * sin(theta);
+            particles[i].z = sphere_radius * cos(phi);
+            particles[i].x_prev = particles[i].x;
+            particles[i].y_prev = particles[i].y;
+            particles[i].z_prev = particles[i].z;
+            particles[i].ax = 0.0f;
+            particles[i].ay = 0.0f;
+            particles[i].az = 0.0f;
+            particles[i].radius = 0.03f;
+        }
+        return;
+    }
+
+    int i;
+    for (i = 0; i < num_particles; i++) {
         particles[i].x = ((float)rand() / RAND_MAX) * 2.0f - 1.0f;
         particles[i].y = ((float)rand() / RAND_MAX) * 2.0f - 1.0f;
         particles[i].z = ((float)rand() / RAND_MAX) * 2.0f - 1.0f;
@@ -119,7 +145,7 @@ void initialize_particles(Particle* particles, int num_particles) {
         particles[i].ax = 0.0f;
         particles[i].ay = 0.0f;
         particles[i].az = 0.0f;
-        particles[i].radius = 0.03f;
+        particles[i].radius = 0.05f;
     }
 }
 
@@ -175,28 +201,57 @@ void fix_position(Particle *p1, Particle *p2) {
 
 void update_particles(Particle* particles, int num_particles, float dt) {
     // apply forces to particles
-
-    for (int i = 0; i < num_particles; i++) {
-        particles[i].ay += GRAVITY;
+    int a;
+    for (a = 0; a < num_particles; a++) {
+        particles[a].ay += GRAVITY;
     }
     
     // handle collisions
-
-    for (int i = 0; i < num_particles; i++) {
-        for (int j = 0; j < num_particles; j++) {
-            if (i != j) {
-                if (detect_collision(&particles[i], &particles[j])) {
-                    fix_position(&particles[i], &particles[j]);
+    int j, k;
+    for (j = 0; j < num_particles; j++) {
+        for (k = 0; k < num_particles; k++) {
+            if (j != k) {
+                if (detect_collision(&particles[j], &particles[k])) {
+                    fix_position(&particles[j], &particles[k]);
                 }
             }
         }
     }
 
     // apply the constraints of the boundary
-    
-    for (int i = 0; i < num_particles; i++) {
-        if (boolsphere) {
-            continue; // fix this later on to handle sphere
+    int i;
+    for (i = 0; i < num_particles; i++) {
+        if (border == "sphere") {
+            float distance = sqrt(particles[i].x * particles[i].x +
+                                  particles[i].y * particles[i].y +
+                                  particles[i].z * particles[i].z);
+
+            if (distance + particles[i].radius > sphere_radius) {
+                // Normalize the position vector and move the particle back to the sphere surface
+                float norm_x = particles[i].x / distance;
+                float norm_y = particles[i].y / distance;
+                float norm_z = particles[i].z / distance;
+
+                particles[i].x = norm_x * (sphere_radius - particles[i].radius);
+                particles[i].y = norm_y * (sphere_radius - particles[i].radius);
+                particles[i].z = norm_z * (sphere_radius - particles[i].radius);
+
+                // Reflect the velocity
+                float vx = (particles[i].x - particles[i].x_prev) / dt;
+                float vy = (particles[i].y - particles[i].y_prev) / dt;
+                float vz = (particles[i].z - particles[i].z_prev) / dt;
+
+                float dot_product = vx * norm_x + vy * norm_y + vz * norm_z;
+
+                vx -= 2 * dot_product * norm_x;
+                vy -= 2 * dot_product * norm_y;
+                vz -= 2 * dot_product * norm_z;
+
+                particles[i].x_prev = particles[i].x - vx * dt;
+                particles[i].y_prev = particles[i].y - vy * dt;
+                particles[i].z_prev = particles[i].z - vz * dt;
+            }
+            // continue; // fix this later on to handle sphere
         } else {
             if (particles[i].x < -1.0f + particles[i].radius) {
                 particles[i].x = -1.0f + particles[i].radius;
@@ -225,27 +280,28 @@ void update_particles(Particle* particles, int num_particles, float dt) {
     }
 
     // now we update our positions and acceleration
-    for (int i = 0; i < num_particles; i++) {
-        float disp_x = particles[i].x - particles[i].x_prev;
-        float disp_y = particles[i].y - particles[i].y_prev;
-        float disp_z = particles[i].z - particles[i].z_prev;
+    int ii;
+    for (ii = 0; ii < num_particles; ii++) {
+        float disp_x = particles[ii].x - particles[ii].x_prev;
+        float disp_y = particles[ii].y - particles[ii].y_prev;
+        float disp_z = particles[ii].z - particles[ii].z_prev;
 
-        particles[i].x_prev = particles[i].x;
-        particles[i].y_prev = particles[i].y;
-        particles[i].z_prev = particles[i].z;
+        particles[ii].x_prev = particles[ii].x;
+        particles[ii].y_prev = particles[ii].y;
+        particles[ii].z_prev = particles[ii].z;
 
-        particles[i].ax *= dt * dt;
-        particles[i].ay *= dt * dt;
-        particles[i].az *= dt * dt;
+        particles[ii].ax *= dt * dt;
+        particles[ii].ay *= dt * dt;
+        particles[ii].az *= dt * dt;
 
-        particles[i].x += disp_x + particles[i].ax;
-        particles[i].y += disp_y + particles[i].ay;
-        particles[i].z += disp_z + particles[i].az;
+        particles[ii].x += disp_x + particles[ii].ax;
+        particles[ii].y += disp_y + particles[ii].ay;
+        particles[ii].z += disp_z + particles[ii].az;
 
         // reset acceleration
-        particles[i].ax = 0.0f;
-        particles[i].ay = 0.0f;
-        particles[i].az = 0.0f;
+        particles[ii].ax = 0.0f;
+        particles[ii].ay = 0.0f;
+        particles[ii].az = 0.0f;
     }
 
 
@@ -253,18 +309,24 @@ void update_particles(Particle* particles, int num_particles, float dt) {
 
 void draw_particles(Particle* particles, int num_particles) {
     GLUquadricObj* quadric = gluNewQuadric();
-    for (int i = 0; i < num_particles; i++) {
+    int i;
+    for (i = 0; i < num_particles; i++) {
         glPushMatrix();
         glTranslatef(particles[i].x, particles[i].y, particles[i].z);
         
         // interpolate color
-        float acc_magnitude = sqrt(particles[i].ax * particles[i].ax +
-                                   particles[i].ay * particles[i].ay +
-                                   particles[i].az * particles[i].az);
 
-        float t = (acc_magnitude - 0.01f) / (0.05f - 0.01f); 
-        if (t > 1.0f) t = 1.0f;
-        if (t < 0.0f) t = 0.0f;
+        float vel_x = (particles[i].x - particles[i].x_prev) / time_step;
+        float vel_y = (particles[i].y - particles[i].y_prev) / time_step;
+        float vel_z = (particles[i].z - particles[i].z_prev) / time_step;
+
+        // Calculate the magnitude of the velocity
+        float vel_magnitude = sqrt(vel_x * vel_x + vel_y * vel_y + vel_z * vel_z);
+
+        float t = fabs(vel_magnitude) / (3.0f); 
+        if (t > 1.0f) {
+            t = 1.0f;
+        }
 
         float r, g, b;
         interpolate_color(t, &r, &g, &b);
@@ -278,7 +340,7 @@ void draw_particles(Particle* particles, int num_particles) {
 }
 
 void draw_boundary(){
-    if (boolsphere) {
+    if (border == "sphere") {
         GLUquadric* quad = gluNewQuadric();
         glColor4f(1.0f, 1.0f, 1.0f, 0.1f); 
         gluQuadricDrawStyle(quad, GLU_LINE); 
@@ -343,13 +405,17 @@ void draw() {
     
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluPerspective(45.0, 800.0 / 600.0, 1.0, 100.0);
+    if(border == "sphere") {
+        gluPerspective(0.0, 800.0 / 600.0, 0.1, 100.0);
+    }
+    gluPerspective(45.0, 800.0 / 600.0, 0.1, 100.0);
+    
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-    if (boolsphere) {
-        gluLookAt(2.0, 2.0, 2.0,  // Camera position
+    if (border == "sphere") {
+        gluLookAt(0.0, 0.0, 3.0,  // Camera position
           0.0, 0.0, 0.0,  // Look-at point
-          0.0, 0.0, 1.0); // Up direction
+          0.0, 1.0, 0.0); // Up direction
     } else {
         gluLookAt(3.0, 3.0, 3.0,  // Camera position
             0.0, 0.0, 0.0,  // Look-at point
